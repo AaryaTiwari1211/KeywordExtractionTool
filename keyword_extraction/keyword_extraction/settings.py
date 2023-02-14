b2180533f5a2c1f67e49e8666cbc14316fba5293
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-02-14 12:48:39 +0530

    Navbar Added

--- keyword_extraction/keyword_extraction/settings.py
+++ keyword_extraction/keyword_extraction/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
