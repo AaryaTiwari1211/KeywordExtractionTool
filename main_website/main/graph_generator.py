d686f2e902adbf35000e87518d2186b49ffd137f
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-04-30 18:03:06 +0530

    New Commits

--- main_website/main/graph_generator.py
+++ main_website/main/graph_generator.py
@@ -2,18 +2,18 @@ import matplotlib.pyplot as plt2
 
 def graph_generator(key,freq):
     
-    fig , ax = plt2.subplots(1,2 , figsize = (10,5))
+    fig , ax = plt2.subplots(1,2 , figsize = (30,5))
     ax1 = ax[0]
     ax2 = ax[1]
     ax1.plot(key,freq)
     ax1.title.set_text("Line Visualization")
     for tick in ax1.get_xticklabels():
-        tick.set_rotation(63)
+        tick.set_rotation(90)
     
     ax2.bar(key, freq, color='maroon', width=0.4)
     ax2.title.set_text("Bar Visualization")
     
     for tick2 in ax2.get_xticklabels():
-        tick2.set_rotation(63)
+        tick2.set_rotation(90)
         
     fig.savefig("media/images/graph.png")
